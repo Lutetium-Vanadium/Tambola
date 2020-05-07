99d3a5fb8de1ca3c23e0ca33360c3718500cc135
Lutetium-Vanadium <luv.s7000@gmail.com>
2020-05-07 20:06:30 +0530

    Added hack, method to prevent remounting for first route change

--- Frontend/src/App/App.tsx
+++ Frontend/src/App/App.tsx
@@ -1,15 +1,30 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Switch, Route, useHistory } from "react-router-dom";
 import PageAnimation from "react-page-animation";
 
 import Home from "./Home";
 import Game from "./Game";
 
+function Null() {
+  return <div></div>;
+}
+
 function App() {
+  // hacky stuff to prevent first remount
+  const history = useHistory();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    history.push("/random-hack");
+    history.goBack();
+    setMounted(true);
+  }, []);
+
   return (
     <div>
-      <PageAnimation classExtension="pages" grid={[[/\/$/, /\/create$/, /^\/[a-z0-9]+/i]]} timeout={300} animate={false}>
+      <PageAnimation classExtension="pages" grid={[[/\/$/, /^\/[a-z0-9]+/i]]} timeout={300} animate={mounted}>
         <Switch>
+          <Route path="/random-hack" component={Null} />
           <Route path="/:id" component={Game} />
           <Route path="/" component={Home} />
         </Switch>
